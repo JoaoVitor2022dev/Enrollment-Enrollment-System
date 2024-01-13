d613ea58ab6a09f9fd061d2ba34ca51dd2d392e7
Joao Vitor <githubconta@gmail.com>
2024-01-13 13:39:37 -0300

    Sistema de matricula de alunos

---
+++ Entities/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercicio_Generic.Entities
+{
+    internal class Aluno
+    {
+        public int Matricula { get; set; }
+
+        public Aluno(int matricula)
+        {
+            Matricula = matricula;
+        }
+        public override int GetHashCode()
+        {
+            return Matricula.GetHashCode();
+        }
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Aluno))
+            {
+                return false; 
+            }
+
+            Aluno other = obj as Aluno;
+            return Matricula.Equals(other.Matricula); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace Exercicio_Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<int> cursoA = new HashSet<int>();
+            HashSet<int> cursoB = new HashSet<int>();
+            HashSet<int> cursoC = new HashSet<int>();
+
+            Console.Write("How many students for A? "); 
+            int n = int.Parse(Console.ReadLine());
+
+           
+            for (int i = 0; i < n; i++)
+            {
+                int matricula = int.Parse(Console.ReadLine());
+
+                cursoA.Add(matricula);
+            }
+
+            Console.Write("How many students for B? ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                int matricula = int.Parse(Console.ReadLine());
+
+                cursoB.Add(matricula);
+            }
+
+            Console.Write("How many students for C? ");
+            int y = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < y; i++)
+            {
+                int matricula = int.Parse(Console.ReadLine());
+
+                cursoC.Add(matricula);
+            }
+
+            HashSet<int> all = new HashSet<int>(cursoA);
+            all.UnionWith(cursoB);
+            all.UnionWith(cursoC);
+
+            Console.WriteLine($"Total students: {all.Count}"); 
+        }
+    }
+}
